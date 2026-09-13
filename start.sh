1b822f57712208c5c41f03d2f561cd4666e66634
danisuarezinef-ai <danisuarezinef@gmail.com>
2026-09-13 23:57:27 +0200

    Advance PAPER runtime to v18 tasks 81-90

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-# v17 composes v16 intelligence with tasks 71-80
-exec python cloud_service_v17.py
+# v18 composes v17 portfolio intelligence with tasks 81-90
+exec python cloud_service_v18.py
